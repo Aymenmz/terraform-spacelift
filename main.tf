21971365204bd77dae6b59de57364a2e6ccd4277
aymenmaiziz <aymenmaiziz55@gmail.com>
2023-10-23 20:30:02 +0100

    fix key name

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "maiziz-compute" {
   subnet_id     = module.networking.subnet_id
   sg_id         = [module.networking.sg_id]
   host_os       = var.host_os
-  key_name      = "my_key_ec2"
+  key_name      = "my_key_ec2_2"
   node_name     = "maiziz"
   instance_type = "t2.micro"
 
