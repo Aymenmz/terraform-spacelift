a1a643de31014a3a6b145574517661b75dd679ca
aymenmaiziz <aymenmaiziz55@gmail.com>
2023-10-23 11:33:03 +0100

    formatting

--- outputs.tf
+++ outputs.tf
@@ -4,11 +4,11 @@ output "public_ip" {
 
 output "connection_script" {
 
-    value = templatefile("${var.host_os}-ssh-config.tpl", {
-        hostname     = aws_instance.dev_ec2.public_ip
-        user         = "ubuntu"
-        identityFile = "~/.ssh/my_key_ec2"
+  value = templatefile("${var.host_os}-ssh-config.tpl", {
+    hostname     = aws_instance.dev_ec2.public_ip
+    user         = "ubuntu"
+    identityFile = "~/.ssh/my_key_ec2"
 
   })
-  
+
 }
